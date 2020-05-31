bad616b0d1619721e3842771560515f4a0a6c6d2
TeonMehta <teonmehta@gmail.com>
2020-05-31 11:27:34 -0400

    added pages and links to pages

---
+++ src/pages/topics.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const TopicsPage = () => (
+    <Layout>
+        <SEO title="Topics" />
+        <h1>TOPICS</h1>
+
+    </Layout>
+)
+
+export default TopicsPage
